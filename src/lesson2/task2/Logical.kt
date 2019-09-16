3f7941bb71d59c9c01b285d10274e7e8ba51163b
vitekkor <korotkih.vd@edu.spbstu.ru>
2019-09-16 20:33:32 +0300

    Новый вариант brickPasses

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -64,6 +64,9 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = ((a <= r) && (b <= s || c <= s)) ||
-        ((b <= r) && (a <= s || c <= s)) ||
-        ((c <= r) && (a <= s || b <= s))
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = when (minOf(a, b, c)) {
+    a -> a <= minOf(r, s) && minOf(b, c) <= maxOf(r, s)
+    b -> b <= minOf(r, s) && minOf(a, c) <= maxOf(r, s)
+    else -> c <= minOf(r, s) && minOf(a, b) <= maxOf(r, s)
+
+}
