3ba04c5c8bb72bd9d278627b4a0bbd0bdc8eb184
vitekkor <korotkih.vd@edu.spbstu.ru>
2019-10-02 16:52:54 +0300

    isCoPrime переделал

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -139,12 +139,7 @@ fun maxDivisor(n: Int): Int = if (isPrime(n)) 1 else n / minDivisor(n)
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean {
-    for (i in 2..max(m, n) / 2) {
-        if (m % i == 0 && n % i == 0) return false
-    }
-    return true
-}
+fun isCoPrime(m: Int, n: Int): Boolean = max(m, n) % minDivisor(min(m, n)) != 0
 
 /**
  * Простая
