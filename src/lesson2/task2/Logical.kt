56dc37b90ca70da8476b2211bd27d6a1f2e12cef
vitekkor <korotkih.vd@edu.spbstu.ru>
2019-10-16 18:03:03 +0300

    Omg, damn kotoed. 2nd lesson not has been passed tests while I already on 5th

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,7 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import lesson1.task1.trackLength
 import kotlin.math.abs
 
 /**
@@ -53,7 +54,7 @@ fun daysInMonth(month: Int, year: Int): Int = when (month) {
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = (sqr(x1 - x2) + sqr(y1 - y2) <= sqr(r1 - r2)) && (r2 >= r1)
+): Boolean = trackLength(x1, y1, x2, y2) + r1 <= r2
 
 /**
  * Средняя
