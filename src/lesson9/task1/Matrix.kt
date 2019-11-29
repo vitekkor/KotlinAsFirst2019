525f5c40dda3502c084dddc329ba3222de850b2e
vitekkor <korotkih.vd@edu.spbstu.ru>
2019-11-30 00:33:49 +0300

    Matrices 6.8.2

--- src/lesson9/task1/Matrix.kt
+++ src/lesson9/task1/Matrix.kt
@@ -35,7 +35,6 @@ interface Matrix<E> {
     operator fun set(row: Int, column: Int, value: E)
 
     operator fun set(cell: Cell, value: E)
-    infix fun indexOf(element: E): Cell
 }
 
 /**
@@ -89,7 +88,7 @@ data class MatrixImpl<E>(override val height: Int, override val width: Int, val 
     }
 
     override fun hashCode(): Int = list.hashCode()
-    override fun indexOf(element: E): Cell {
+    infix fun indexOf(element: E): Cell {
         for (i in 0 until height) {
             val j = list[i].indexOf(element)
             if (j != -1) return Cell(i, j)
